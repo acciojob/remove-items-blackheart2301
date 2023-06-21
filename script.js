0b63a0b83b0de85155dfa190d87dfa0b9b78c03c
blackheart2301 <128833356+blackheart2301@users.noreply.github.com>
2023-06-21 20:33:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+const drop= document.getElementbyId("colorSelect");
+
+function remove(){
+     if(drop.textContent=="Red"){
+		 let choosen= drop.getElementbyId(first);
+		 drop.removeChild('choosen');
+	 }
+	if(drop.textContent=="Green"){
+		 let choosen= drop.getElementbyId(second);
+		 drop.removeChild('choosen');
+	}
+	if(drop.textContent=="White"){
+		let choosen= drop.getElementbyId(second);
+		 drop.removeChild('choosen');
+	}	 
+	if(drop.textContent=="Black"){
+		let choosen= drop.getElementbyId(second);
+		 drop.removeChild('choosen');
+	}
+}
