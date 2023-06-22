0a6f5e56450fbc763bfb8706d16e266c708b6fd9
blackheart2301 <128833356+blackheart2301@users.noreply.github.com>
2023-06-22 08:47:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
 //your JS code here. If required.
-
-const drop = document.querySelector('#colorSelect');
-let output = drop.value;
-
 function remove(){
-     if(output=="Red" && drop.hasChildNodes()){
-		 drop.removeChild(drop.children[0]);
-	 }
-	if(output=="Green" && drop.hasChildNodes()){ 
-		 drop.removeChild(drop.children[1]);
-	}
-	if(output=="White" && drop.hasChildNodes()){
-		 drop.removeChild(drop.children[2]);
-	}	 
-	if(output=="Black" && drop.hasChildNodes()){
-		 drop.removeChild(drop.children[3]);
-	}
+    const drop = document.querySelector('#colorSelect');
+    drop.remove(drop.selectedIndex);
 }
+
+/*"OR"
+function remove(){
+	$("#colorSelect option[value=""]").remove();
+}*/
